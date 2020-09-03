d60420bcf7a0181e8980871124fada7968c01ec9
bikiniweekend <bikiniweekend@hotmail.com>
2020-09-03 19:22:41 +0200

    new update

--- web-application/src/app/app-routing.module.ts
+++ web-application/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AccountsComponent } from './pages/accounts/accounts.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'accounts', component: AccountsComponent},
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web-application/src/app/app.module.ts
+++ web-application/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AccountsComponent } from './pages/accounts/accounts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
