c3a19c7c1bc52a4131ecc63183228bda2480bb23
bikiniweekend <bikiniweekend@hotmail.com>
2020-09-06 21:18:05 +0200

    new update

--- web-application/src/app/directives/highlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightTopBarDirective } from './highlighttopbar.directive';
-
-describe('HighlightTopBarDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightTopBarDirective();
-    expect(directive).toBeTruthy();
-  });
-});
